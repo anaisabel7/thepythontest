46bb46867c2055c906c0c947aa558ba4198543ec
Ana Isabel <imacswv@gmail.com>
2018-07-17 13:41:36 +0200

    Added basic models for this project

--- thepythontest/questions/models.py
+++ thepythontest/questions/models.py
@@ -6,3 +6,14 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class Question(models.Model):
+    text = models.CharField(max_length=600, unique=True)
+    verified = models.BooleanField(default=False)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
+    text = models.CharField(max_length=400)
+    correct = models.BooleanField(default=False)
