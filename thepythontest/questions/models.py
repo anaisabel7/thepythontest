f8ea5b8780f4e138b0b56aa677cfc020ca33a601
Ana Isabel <imacswv@gmail.com>
2018-07-19 11:59:00 +0200

    Add score to user and creator to question models

--- thepythontest/questions/models.py
+++ thepythontest/questions/models.py
@@ -1,16 +1,16 @@
 from django.contrib.auth.models import AbstractUser
+from django.contrib.auth import get_user_model
 from django.db import models
-
-# Create your models here.
 
 
 class User(AbstractUser):
-    pass
+    score = models.IntegerField(default=0)
 
 
 class Question(models.Model):
     text = models.CharField(max_length=600, unique=True)
     verified = models.BooleanField(default=False)
+    creator = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
 
 
 class Answer(models.Model):
