d0a8be2e4c07353383e9ba36a0d6d2b1cf5f58f6
Ana Isabel <imacswv@gmail.com>
2018-07-19 12:17:11 +0200

    Registering models in admin

--- thepythontest/questions/admin.py
+++ thepythontest/questions/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from questions.models import User, Question, Answer
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Question)
+admin.site.register(Answer)
